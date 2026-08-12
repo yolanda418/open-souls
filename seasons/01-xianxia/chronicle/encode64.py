545afb1ad74708f50f750dc3e59bbfe31575909f
yolanda418 <yolande3113zyl@gmail.com>
2026-08-11 20:12:23 -0600

    clean

--- seasons/01-xianxia/chronicle/encode64.py
+++
@@ -1,4 +0,0 @@
-﻿import sys, base64
-data = sys.stdin.buffer.read()
-encoded = base64.b64encode(data).decode('ascii')
-sys.stdout.write(encoded)
--- seasons/01-xianxia/chronicle/escape.py
+++
@@ -1,9 +0,0 @@
-﻿import sys
-data = sys.stdin.buffer.read()
-text = data.decode('utf-8')
-def esc(c):
-    o = ord(c)
-    if o - 0x7f - 1: return c
-    return '\\\\u' + format(o, '04x')
-escaped = ''.join(esc(c) for c in text)
-sys.stdout.buffer.write(escaped.encode('utf-8'))
